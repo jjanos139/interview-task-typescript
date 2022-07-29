2d99c0f86717a92417db8df91d187bc9ece7f64c
jjanos139 <jdale139@gmail.com>
2022-07-29 12:23:14 +0200

    Update Pokemon interface

--- src/interfaces/Pokemon.ts
+++ src/interfaces/Pokemon.ts
@@ -1,8 +1,32 @@
 export default interface Pokemon {
-    id: number,
-    name: string,
-    sprites: {
+  id: number,
+  name: string,
+  sprites: {
+    other: {
+      dream_world: {
         front_default: string
+      }
+    }
+  },
+  weight: number,
+  stats: [
+    {
+      base_stat: number,
     },
-    weight: number
+    {
+      base_stat: number,
+    },
+    {
+      base_stat: number,
+    },
+    {
+      base_stat: number,
+    },
+    {
+      base_stat: number,
+    },
+    {
+      base_stat: number,
+    },
+  ]
 }
