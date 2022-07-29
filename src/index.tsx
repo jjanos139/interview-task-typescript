343bac7ce41f0f16782d16672902054750df107d
jjanos139 <jdale139@gmail.com>
2022-07-29 16:16:40 +0200

    Add stylesheet

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import './styles/main.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -11,8 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-);
+  </React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
